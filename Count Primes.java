c6e57222f527cf9bf5ce0a8faee041926a2a8b31
Prakhar Agarwal <prakhar@everything.net.in>
2021-10-01 17:56:16 +0530

    Create Count Primes.java

---
+++ Count Primes.java
@@ -0,0 +1,23 @@
+boolean prime[] = new boolean[n + 1];
+        for (int i = 0; i <= n; i++)
+            prime[i] = true;
+ 
+        for (int p = 2; p * p <= n; p++)
+        {
+            // If prime[p] is not changed, then it is a
+            // prime
+            if (prime[p] == true)
+            {
+                // Update all multiples of p
+                for (int i = p * p; i <= n; i += p)
+                    prime[i] = false;
+            }
+        }
+ 
+        // Print all prime numbers
+        for (int i = 2; i <= n; i++)
+        {
+            if (prime[i] == true)
+                System.out.print(i + " ");
+        }
+    }
