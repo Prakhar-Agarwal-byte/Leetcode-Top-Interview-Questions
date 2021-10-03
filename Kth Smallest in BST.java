f2f085c2bd68e3b0ed28b34992347b44a7f8234d
Prakhar Agarwal <prakhar@everything.net.in>
2021-10-03 18:23:21 +0530

    Create Kth Smallest in BST.java

---
+++ Kth Smallest in BST.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        int[] nums = new int[2];
+        inorder(root, nums, k);
+
+        return nums[1];
+    }
+
+    private void inorder(TreeNode root, int[] nums, int k) {
+        if (root == null)
+            return;
+
+        inorder(root.left, nums, k);
+        if (++nums[0] == k) {
+            nums[1] = root.val;
+            return;
+        }
+        inorder(root.right, nums, k);
+    }
+}
